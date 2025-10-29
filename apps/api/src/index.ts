e9af7fe7a4d03201bed779a057d2d24370d1348a
Haris Alicic <haris.alicic1@gmail.com>
2025-10-29 20:08:07 +0100

    init

---
+++ apps/api/src/index.ts
@@ -0,0 +1,25 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+const app = new Hono();
+
+app.use('*', cors());
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/api/data', (c) => {
+  return c.json({ message: 'Hello World' });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
